68d6b5298390948e569ee28b67db5b4d2e702c07
edel1 <ghygg@naver.com>
2018-11-01 18:20:51 +0900

    함수내용변경 ing......

--- FIPM/Get_ROI.cpp
+++ FIPM/Get_ROI.cpp
@@ -1,29 +1,17 @@
 #include "pch.h"
 #include "Get_ROI.h"
 #include <vector>
-using namespace cv;
-using namespace std;
 
-void GetImg(std::string path)
+
+
+
+Mat GetFluorescenceImage(Mat InputImg, int n_threshold, vector<vector<Point>>contours, vector<Vec4i> hierarchy, vector<cv::Rect> bounding_boxes)
 {
-	Mat img = imread(path, 0);
-	
-	imshow("img", img);
-	GetROI(img);
-	
-	
 
-}
-
-void GetROI(Mat img)
-{
-	vector<vector<Point>>contours;
-	vector<Vec4i> hierarchy;
 	Mat outimg,Cannyimg,resultimg;
-	//2진화
-	threshold(img, outimg, 55, 255, THRESH_BINARY);// 재설정 필요... 혹은 다른 방법 적용?
 
-	imshow("img2", outimg);
+	//2진화
+	threshold(InputImg, outimg, n_threshold, 255, THRESH_BINARY);// 재설정 필요... 혹은 다른 방법 적용?
 	//waitKey();
 
 	//Canny(outimg, Cannyimg, 20, 40, 3);
@@ -32,36 +20,38 @@ void GetROI(Mat img)
 	//ROI 추출 후 HAT 씌우기........
 
 	//경계선 추출
-	findContours(outimg, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 255));
+	findContours(outimg, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 255)); // 뭔가 이상하다.... - 이미지 아래로 밀림.....
 	resultimg = Mat::zeros(outimg.size(), CV_8U);
+	vector<vector<Point> > contours_poly (contours.size());
+	 //bounding_boxes = new vector<Rect>(contours.size());
 
-	vector<vector<Point> > contours_poly(contours.size());
-	vector<Rect> boundRect(contours.size());
 	for (int i = 0; i < contours.size(); i++)
 	{
 		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
-		boundRect[i] = boundingRect(Mat(contours_poly[i]));
+		bounding_boxes[i] = boundingRect(Mat(contours_poly[i]));
 	
 	}
-	imshow("img3", outimg);
-	Scalar color = Scalar(255);
-	for (int i = 0; i < contours.size(); i++)
-	{
-		
-		//drawContours(resultimg, contours, i, color,1, 8, hierarchy, 0, Point());
-		// 사각형 그리기..... bounding box
-		rectangle(resultimg, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
 
-	}
-	imshow("img4", resultimg);
-	waitKey();
+	//Main으로 이동...
+	//imshow("img3", outimg);
+	//Scalar color = Scalar(255);
+	//for (int i = 0; i < contours.size(); i++)
+	//{
+	//	
+	//	drawContours(resultimg, contours, i, color,1, 8, hierarchy, 0, Point());// 경계선 그리기
+	//	// 사각형 그리기..... bounding box
+	//	rectangle(resultimg, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0); // 박스 그리기...
+			//}
+	//imshow("img4", resultimg);
+	//waitKey();
 
-	
-	
+
+
 
 	//1. 경계선 추출
 	//2. 사각형 그리기.
 	//3. 이쁘게
+	return resultimg;
 }
 
 void DisplayROI()
--- FIPM/Get_ROI.h
+++ FIPM/Get_ROI.h
@@ -1,11 +1,14 @@
 #pragma once
+#include "pch.h" // 변경 필요....
 #include <opencv2\opencv.hpp>
 
+using namespace cv;
+using namespace std;
 
 
-void GetImg(std::string path);
+Mat GetFluorescenceImage(Mat InputImg,int n_threshold, vector<vector<Point>>contours, vector<Vec4i> hierarchy , vector<cv::Rect> bounding_boxes);
 
-void GetROI(cv::Mat img);
 
-void DisplayROI();
 
+//todo....
+//함수 이름변경........
